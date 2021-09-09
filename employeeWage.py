2c1658da449f648b49e7103e1667b8c099758b01
bindubhargavi568 <bindubhargavi568@gmail.com>
2021-09-09 10:07:23 +0530

    Calculated:PArttime employee wage

--- employeeWage.py
+++ employeeWage.py
@@ -1,17 +1,21 @@
 import random
 
 wage_per_hr = 20
-emp_check = int(random.randint(0, 1))
+emp_check = int(random.randint(0, 2))
 
 
 def employee_attendance():
     if emp_check == 1:
         print("Employee is present")
         full_day_hrs = 8
+        emp_wage = full_day_hrs * wage_per_hr
+    elif emp_check == 2:
+        print("Employee worked as part-time")
+        part_time_hrs = 4
+        emp_wage = part_time_hrs * wage_per_hr
     else:
         print("Employee is absent")
-        full_day_hrs = 0
-    emp_wage = full_day_hrs * wage_per_hr
+        emp_wage = 0
     print("Employee wage is: ", emp_wage)
 
 
