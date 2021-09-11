50d4da437db11218c95dc0491d98c593673cdcdc
bindubhargavi568 <bindubhargavi568@gmail.com>
2021-09-11 16:20:54 +0530

    Used:class and objects

--- employeeWage.py
+++ employeeWage.py
@@ -8,22 +8,26 @@ part_time_hrs = 4
 absent_hrs = 0
 
 
-def wage_calculation():
-    working_days = 0
-    working_hrs = 0
-    employee_attendance = {
-        1: full_time_hrs,
-        2: part_time_hrs,
-        0: absent_hrs
-    }
-    while working_hrs <= total_working_hrs and working_days < total_working_days:
-        emp_check = int(random.randint(0, 2))
-        working_hrs = working_hrs + employee_attendance.get(emp_check)
-        working_days = working_days + 1
-        total_wage = working_hrs * wage_per_hr
+class EmployeeWage:
 
-    return total_wage
+    def wage_calculation(self):
+        working_days = 0
+        working_hrs = 0
+        employee_attendance = {
+            1: full_time_hrs,
+            2: part_time_hrs,
+            0: absent_hrs
+        }
+        # employee_attendance = int(input("Enter Employee Attendance type"))
+        while working_hrs <= total_working_hrs and working_days < total_working_days:
+            emp_check = int(random.randint(0, 2))
+            working_hrs = working_hrs + employee_attendance.get(emp_check)
+            working_days = working_days + 1
+            total_wage = working_hrs * wage_per_hr
+
+        print("total wage:", total_wage)
 
 
 if __name__ == "__main__":
-    print("Monthly wage: ", wage_calculation())
+    employee = EmployeeWage()
+    employee.wage_calculation()
